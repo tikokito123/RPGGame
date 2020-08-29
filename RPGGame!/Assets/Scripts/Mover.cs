23ff4010dfe4af4c8438c01b730e57424cf617e7
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-30 02:26:39 +0300

    Moving Animation!

--- RPGGame!/Assets/Scripts/Mover.cs
+++ RPGGame!/Assets/Scripts/Mover.cs
@@ -17,8 +17,8 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
-
     private void MoveToCursor()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -28,5 +28,12 @@ public class Mover : MonoBehaviour
         {
             navMesh.destination = hit.point;
         }
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
     }
 }
