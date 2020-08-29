f05f91e641aa196c64798a39ad89dab4da71aab8
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-30 00:28:29 +0300

    Player move towards enemy!

---
+++ RPGGame!/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMesh;
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        navMesh.destination = target.position;
+    }
+}
