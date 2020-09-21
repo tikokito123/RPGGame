bd79ef644109f9dedb0cc6414fa9b8d8ac48f565
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-09-21 06:09:13 +0300

    Fireball weapon function... bug fixed...

--- RPGGame!/Assets/Scripts/Combat/Projectile.cs
+++ RPGGame!/Assets/Scripts/Combat/Projectile.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Core;
 using UnityEditor;
+using UnityEngine.XR.WSA.Input;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 0.5f;
+    [SerializeField] bool isHoming = true;
     Health target = null;
     float damage = 0;
+    private void Start()
+    {
+        transform.LookAt(GetAimLoacation());
+    }
     void Update()
     {
         if (target == null) return;
-        transform.LookAt(GetAimLoacation());
+        if (isHoming && !target.IsDead())
+        {
+            transform.LookAt(GetAimLoacation());
+        }
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
     public void SetTarget(Health target, float damage)
@@ -30,7 +39,8 @@ public class Projectile : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Health>() != target) return;
+        if (target.IsDead()) return;
         target.HealthDamage(damage);
-        Destroy(gameObject, 0.45f);
+        Destroy(gameObject);
     }
 }
