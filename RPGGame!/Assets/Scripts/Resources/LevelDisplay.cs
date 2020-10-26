74fae532b60e84e5b06d0c7eca138153fef540dc
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-10-26 13:40:56 +0200

    Display Level!

---
+++ RPGGame!/Assets/Scripts/Resources/LevelDisplay.cs
@@ -0,0 +1,18 @@
+﻿using RPG.Stats;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDisplay : MonoBehaviour
+{
+    BaseStats level;
+    void Awake()
+    {
+        level = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+    }
+    void Update()
+    {
+        GetComponent<Text>().text = level.GetLevel().ToString();
+    }
+}
