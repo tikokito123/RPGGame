4b9f73f8961c7280535ff542daaa33f418b07c14
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-11-07 04:33:12 +0200

    Health Bar working

--- RPGGame!/Assets/Scripts/Attributes/HealthBar.cs
+++ RPGGame!/Assets/Scripts/Attributes/HealthBar.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,22 @@ namespace RPG.Attributes
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] Health health = null;
+        [SerializeField] Canvas rootCanvas = null;
         [SerializeField] RectTransform forground = null;
         void Update()
         {
+            rootCanvas.enabled = true;
+            CheckRootCanvas();
             forground.localScale = new Vector3(health.GetFranction(), 1, 1);
+        }
+
+        private void CheckRootCanvas()
+        {
+            if (Mathf.Approximately(health.GetFranction(), 0) || Mathf.Approximately(health.GetFranction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
         }
     }
 }
